2233dacd3441b88160c5eb9af85ee0366523cf13
Paul Pollack <pdpman88@gmail.com>
2016-10-23 18:55:13 -0400

    set up basic file scanning

--- spaghetti.go
+++ spaghetti.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	//"flag"
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"sync"
 )
+
+type result struct {
+	path     string
+	snippets string
+	err      error
+}
 
 func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
 	paths := make(chan string)
@@ -20,7 +30,9 @@ func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) 
 				return err
 			}
 
-			if info.Mode().IsDir() {
+			is_python_source, err := regexp.MatchString(`[a-z]*\.py$`, path)
+
+			if !is_python_source {
 				return nil
 			}
 
@@ -37,37 +49,65 @@ func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) 
 	return paths, errc
 }
 
-func searcher(done <-chan struct{}, target string, paths <-chan string, c chan<- []string) {
+func searcher(id int, done <-chan struct{}, target string, paths <-chan string, c chan<- result) {
 	for path := range paths {
-		data := MethodSearch(target, path)
+		f, err := os.Open(path)
+
+		var snippets_buffer bytes.Buffer
+		var buffer bytes.Buffer
+
+		scanner := bufio.NewScanner(f)
+		is_target_stub := false
+		for scanner.Scan() {
+			line := scanner.Text()
+			// TODO: need to compare line with regexp for decorator
+			if strings.Contains(line, "def") && is_target_stub {
+				is_target_stub = false
+				fmt.Printf("I'm searcher %d and I found a target in %s \n", id, path)
+				buffer.WriteString("\n")
+				snippets_buffer.WriteString(buffer.String())
+				buffer.Reset()
+			}
+			// When true, we'll add this stub to snippets when we hit the next def
+			if strings.Contains(line, target) {
+				is_target_stub = true
+			}
+			buffer.WriteString(line)
+			buffer.WriteString("\n")
+		}
+		if is_target_stub {
+			fmt.Printf("I'm searcher %d and I found a target in %s \n", id, path)
+			snippets_buffer.WriteString(buffer.String())
+			buffer.Reset()
+		}
+
+		snippets := snippets_buffer.String()
+
+		if len(snippets) == 0 {
+			continue
+		}
+
+		result := result{
+			path,
+			snippets,
+			err,
+		}
 
 		select {
-		case c <- data:
+		case c <- result:
 		case <-done:
 			return
 		}
 	}
 }
 
-func MethodSearch(methodName string, path string) (results []string) {
-	//f, err := os.Open(path)
-
-	/*if err != nil {
-		return err
-	}*/
-
-	result := []string{methodName, path}
-	return result
-}
-
 func main() {
-	/*args := os.Args[1:]
+	args := os.Args[1:]
 
+	var target string
 	if len(args) > 0 {
-		fmt.Println("Got some args: ", args)
-	} else {
-		fmt.Println("No args here!")
-	}*/
+		target = args[0]
+	}
 
 	pwd, _ := os.Getwd()
 
@@ -75,9 +115,8 @@ func main() {
 	defer close(done)
 
 	paths, errc := walkFiles(done, pwd)
-	target := "test"
 
-	c := make(chan []string)
+	c := make(chan result)
 
 	var wg sync.WaitGroup
 	const numSearchers = 10
@@ -85,17 +124,19 @@ func main() {
 
 	for i := 0; i < numSearchers; i++ {
 		go func() {
-			searcher(done, target, paths, c)
+			searcher(i, done, target, paths, c)
 			wg.Done()
 		}()
 	}
+
 	go func() {
 		wg.Wait()
 		close(c)
 	}()
 
-	for dirList := range c {
-		fmt.Println(dirList)
+	for result := range c {
+		fmt.Println(result.path)
+		fmt.Println(result.snippets)
 	}
 
 	if err := <-errc; err != nil {
