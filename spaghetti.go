3f669aea43693a7a1c79bb00d0de6abe1cfc1cdb
Paul Pollack <pdpman88@gmail.com>
2016-10-25 22:31:07 -0400

    support exclude patterns and tinker with concurrency

--- spaghetti.go
+++ spaghetti.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	//"flag"
 	"bufio"
 	"bytes"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
 	"sync"
 )
@@ -19,7 +20,14 @@ type result struct {
 	err      error
 }
 
-func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+// Declaring command-line flags
+var exclude_patterns string
+
+func init() {
+	flag.StringVar(&exclude_patterns, "exclude_patterns", "", "A comma-separated list of files to be exlcuded")
+}
+
+func walkFiles(done <-chan struct{}, root string, exclude_patterns string) (<-chan string, <-chan error) {
 	paths := make(chan string)
 	errc := make(chan error, 1)
 
@@ -30,10 +38,23 @@ func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) 
 				return err
 			}
 
+			// Checking to see if the file ends in .py
 			is_python_source, err := regexp.MatchString(`[a-z]*\.py$`, path)
+
+			if err != nil {
+				return err
+			}
 
 			if !is_python_source {
 				return nil
+			}
+
+			if len(exclude_patterns) > 0 {
+				for _, pattern := range strings.Split(exclude_patterns, ",") {
+					if strings.Contains(path, pattern) {
+						return nil
+					}
+				}
 			}
 
 			select {
@@ -49,7 +70,7 @@ func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) 
 	return paths, errc
 }
 
-func searcher(id int, done <-chan struct{}, target string, paths <-chan string, c chan<- result) {
+func searcher(done <-chan struct{}, target string, paths <-chan string, results chan<- result) {
 	for path := range paths {
 		f, err := os.Open(path)
 
@@ -60,12 +81,15 @@ func searcher(id int, done <-chan struct{}, target string, paths <-chan string, 
 		is_target_stub := false
 		for scanner.Scan() {
 			line := scanner.Text()
-			// TODO: need to compare line with regexp for decorator
-			if strings.Contains(line, "def") && is_target_stub {
-				is_target_stub = false
-				fmt.Printf("I'm searcher %d and I found a target in %s \n", id, path)
-				buffer.WriteString("\n")
-				snippets_buffer.WriteString(buffer.String())
+			is_decorated, _ := regexp.MatchString(`^@`, line)
+			is_def, _ := regexp.MatchString(`^\s*def\s.+:$`, line)
+			if is_def || is_decorated {
+				if is_target_stub {
+					is_target_stub = false
+					buffer.WriteString("\n")
+					snippets_buffer.WriteString(buffer.String())
+				}
+
 				buffer.Reset()
 			}
 			// When true, we'll add this stub to snippets when we hit the next def
@@ -76,7 +100,6 @@ func searcher(id int, done <-chan struct{}, target string, paths <-chan string, 
 			buffer.WriteString("\n")
 		}
 		if is_target_stub {
-			fmt.Printf("I'm searcher %d and I found a target in %s \n", id, path)
 			snippets_buffer.WriteString(buffer.String())
 			buffer.Reset()
 		}
@@ -94,7 +117,7 @@ func searcher(id int, done <-chan struct{}, target string, paths <-chan string, 
 		}
 
 		select {
-		case c <- result:
+		case results <- result:
 		case <-done:
 			return
 		}
@@ -102,8 +125,11 @@ func searcher(id int, done <-chan struct{}, target string, paths <-chan string, 
 }
 
 func main() {
-	args := os.Args[1:]
+	runtime.GOMAXPROCS(2)
+	flag.Parse()
+	args := flag.Args()
 
+	// TODO: This arg is required, validate that shit
 	var target string
 	if len(args) > 0 {
 		target = args[0]
@@ -111,30 +137,31 @@ func main() {
 
 	pwd, _ := os.Getwd()
 
+	// Set up channel to alert searchers we're done
 	done := make(chan struct{})
 	defer close(done)
 
-	paths, errc := walkFiles(done, pwd)
+	paths, errc := walkFiles(done, pwd, exclude_patterns)
 
-	c := make(chan result)
+	results := make(chan result)
 
-	var wg sync.WaitGroup
-	const numSearchers = 10
-	wg.Add(numSearchers)
+	var wait_group sync.WaitGroup
+	const numSearchers = 1
+	wait_group.Add(numSearchers)
 
 	for i := 0; i < numSearchers; i++ {
 		go func() {
-			searcher(i, done, target, paths, c)
-			wg.Done()
+			searcher(done, target, paths, results)
+			wait_group.Done()
 		}()
 	}
 
 	go func() {
-		wg.Wait()
-		close(c)
+		wait_group.Wait()
+		close(results)
 	}()
 
-	for result := range c {
+	for result := range results {
 		fmt.Println(result.path)
 		fmt.Println(result.snippets)
 	}
